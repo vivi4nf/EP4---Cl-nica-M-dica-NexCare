0d39a425ce23513bb1a102dda61ce0eba44f12b7
vivi4nf <vivi4nf@gmail.com>
2025-11-28 11:01:33 -0300

    Atualizar o 3_DQL_Consultas.sql

--- 3_DQL_Consultas.sql
+++ 3_DQL_Consultas.sql
@@ -1,34 +1,42 @@
--- 3_DQL_Consultas.sql
+3_DQL_Consultas_atualizado.sql
+
+ Consultas:
 
 USE clinica_nexcare;
 
--- 1) Listar todos os pacientes
+ 1) Listar todos os pacientes
+
 SELECT * FROM paciente;
 
--- 2) Médicos e suas especialidades
-SELECT nome, especialidade FROM medico;
+2) Listar médicos e suas especialidades
+SELECT nome, especialidade 
+FROM medico;
 
--- 3) Consultas agendadas com nome do paciente e médico
+ 3) Consultas agendadas com nome do paciente e médico
 SELECT 
-  c.id_consulta,
-  c.data_consulta,
-  c.hora,
-  p.nome AS paciente,
-  m.nome AS medico
+    c.id_consulta,
+    c.data_consulta,
+    c.hora,
+    p.nome AS paciente,
+    m.nome AS medico
 FROM consulta c
 JOIN paciente p ON c.id_paciente = p.id_paciente
 JOIN medico m ON c.id_medico = m.id_medico
 ORDER BY c.data_consulta, c.hora;
 
--- 4) Pagamentos realizados
-SELECT * FROM pagamento WHERE status_pagamento = 'Pago';
+4) Pagamentos realizados
 
--- 5) Consultas com pagamento (LEFT JOIN para incluir consultas sem pagamento)
+SELECT * FROM pagamento
+WHERE status_pagamento = 'Pago'
+ORDER BY data_pagamento;
+
+5) Consultas com ou sem pagamento
 SELECT 
-  c.id_consulta,
-  p.nome AS paciente,
-  pag.valor,
-  pag.status_pagamento
+    c.id_consulta,
+    p.nome AS paciente,
+    pag.valor,
+    pag.status_pagamento
 FROM consulta c
 LEFT JOIN pagamento pag ON c.id_consulta = pag.id_consulta
-LEFT JOIN paciente p ON c.id_paciente = p.id_paciente;
+LEFT JOIN paciente p ON c.id_paciente = p.id_paciente
+ORDER BY c.id_consulta;
